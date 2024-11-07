da1e240df663356b20d780785f5d9f2c0da5cc35
Wise-394 <114738222+Wise-394@users.noreply.github.com>
2024-11-08 07:47:26 +0800

    attack animatios and moving platform

--- scene/player.gd
+++ scene/player.gd
@@ -5,7 +5,13 @@ const SPEED = 150.0
 const JUMP_VELOCITY = -300.0
 
 @onready var animated_sprite = $AnimatedSprite2D
-
+#animation
+var idle = "idle"
+var sword_attack = "sword_attack"
+var run = "run"
+var bow_attack = "bow_attack"
+var isAttackingSword = false
+var isAttackingBow = false
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -17,17 +23,38 @@ func _physics_process(delta):
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("left", "right")
 	
-	#turn based on key press
-	# -1 = left, 1 = right
+	#check direction and apply
 	if direction > 0:
 		animated_sprite.flip_h = false
 	elif direction < 0:
 		animated_sprite.flip_h = true
+	
+	#move the sprite depends on direction
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 	move_and_slide()
+
+	
+	#animations
+	if Input.is_action_just_pressed("lmb") and isAttackingBow == false:
+		animated_sprite.play(sword_attack)
+		isAttackingSword = true
+	elif Input.is_action_just_pressed("rmb") and isAttackingSword == false:
+		animated_sprite.play(bow_attack)
+		isAttackingBow = true
+	elif direction == 0 and not isAttackingSword and not isAttackingBow :
+		animated_sprite.play(idle)
+	elif direction != 0 and not isAttackingSword and not isAttackingBow:
+		animated_sprite.play(run)
+	
+
+
+func _on_animated_sprite_2d_animation_finished():
+	if animated_sprite.animation == sword_attack:
+		isAttackingSword = false
+	if animated_sprite.animation == bow_attack:
+		isAttackingBow = false
