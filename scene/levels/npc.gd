9677105585eb75284360bfe1284459fcc8e6c9c3
Wise-394 <114738222+Wise-394@users.noreply.github.com>
2024-11-27 10:39:38 +0800

    dialog improvement,tutorial

--- scene/levels/npc.gd
+++ scene/levels/npc.gd
@@ -1,13 +1,23 @@
 extends Area2D
 
-
+@onready var npc: Area2D = $"."
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+var playDialogue = true
 
 func _on_body_entered(body: Node2D) -> void:
 	body.playerState = "talking"
-	DialogueManager.show_dialogue_balloon(load("res://dialogue.dialogue"))
-	return
+	if playDialogue == true:
+		DialogueManager.show_dialogue_balloon(load("res://dialogue.dialogue"))
+		playDialogue = false
+		return
 
 
 
 func _on_body_exited(body: Node2D) -> void:
 	body.playerState = "default"
+	print(body.position.x, npc.position.x)
+	if body.position.x > npc.position.x:
+		animated_sprite_2d.flip_h = false
+	else:
+		animated_sprite_2d.flip_h = true
+	playDialogue = true
